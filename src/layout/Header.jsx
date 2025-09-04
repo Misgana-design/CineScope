dacc4a9ecef3e82c70d5c57a349f15dee12911d8
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-05 00:17:52 +0300

    I have added vanilla css to change the font

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom";
-
+import Logo from "../assets/Images/Logo.png";
+import './Header.css'
 export default function Header() {
   return (
     <>
-      <header><img src="" alt="" /></header>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Search">Search</Link>
-        </li>
-        <li>
-          <Link to="/Favorites">Favorites</Link>
-        </li>
-      </ul>
+      <main className="flex justify-between items-center">
+        <header>
+          <img className="w-30 h-30" src={Logo} alt="" />
+        </header>
+        <ul className="flex gap-20 pr-5 font-">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Search">Search</Link>
+          </li>
+          <li>
+            <Link to="/Favorites">Favorites</Link>
+          </li>
+        </ul>
+      </main>
     </>
   );
 }
