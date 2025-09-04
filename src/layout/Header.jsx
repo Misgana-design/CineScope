31e196b6cc82bd430a15308d0f996c1294fae0c0
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-05 01:02:07 +0300

    I completed building the navbar with router I may modify latter

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/Images/Logo.png";
+import Logo2 from '../assets/Images/Logo2.png'
 import './Header.css'
 export default function Header() {
   return (
     <>
-      <main className="flex justify-between items-center">
-        <header>
-          <img className="w-30 h-30" src={Logo} alt="" />
-        </header>
-        <ul className="flex gap-20 pr-5 font-">
+      <main className="flex p-8 justify-between items-center ">
+        <header className="ml-20 text-purple-700 text-3xl">🎬CineScope</header>
+        <ul className="flex gap-20 pr-5  text-xl text-shadow-sm text-shadow">
           <li>
             <Link to="/">Home</Link>
           </li>
