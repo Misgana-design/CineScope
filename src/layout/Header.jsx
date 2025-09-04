3287cb28fd4b18ccd76e6d8ef29487ccf0947c73
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-04 23:47:38 +0300

    I added Image folder

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to='/Search'>Search</Link>
-      </li>
-      <li>
-        <Link to="/Favorites">Favorites</Link>
-      </li>
-    </ul>
+    <>
+      <header><img src="" alt="" /></header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Search">Search</Link>
+        </li>
+        <li>
+          <Link to="/Favorites">Favorites</Link>
+        </li>
+      </ul>
+    </>
   );
 }
