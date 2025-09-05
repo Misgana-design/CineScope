293042c103f2ceef8895e382b00d695013077410
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-05 22:57:06 +0300

    I have taken the BrowserRoute to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import Header from "./layout/Header";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
 import FavoritesPage from "./pages/FavoritesPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Search" element={<SearchPage />} />
-          <Route path="/Favorites" element={<FavoritesPage />} />
-        </Routes>
-      </>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Search" element={<SearchPage />} />
+        <Route path="/Favorites" element={<FavoritesPage />} />
+      </Routes>
+    </>
   );
 }
 
