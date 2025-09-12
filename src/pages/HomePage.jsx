0d9f779d07bd76d36bdcc7b22056863c0c0a67df
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-12 20:43:17 +0300

    I have added height and weight in the style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import fetchTrending from "../hooks/useTMDBApi";
+
+import { fetchTrending, ORIGINAL_IMAGE_BASE_URL } from "../hooks/useTMDBApi"; // good for hero banners
+
 export default function HomePage() {
   const {
     data: movie = [],
@@ -15,13 +17,20 @@ export default function HomePage() {
   });
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Something went wrong</p>;
-
   const heroMovies = movie[0];
   const restMovies = movie.slice(1);
   return (
     <>
       {isFetching && <p>Background refreshing</p>}
-      <div>{heroMovies && <p>{heroMovies.title}</p>}</div>
+      <div>
+        {heroMovies && <p>{heroMovies.title}</p>}
+        <div
+          style={{
+            backgroundImage: `url(${ORIGINAL_IMAGE_BASE_URL}${heroMovies.backdrop_path})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </div>
     </>
   );
 }
