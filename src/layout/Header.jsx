954dd89032627cd8d9fad4b0db1784d9190c4f5f
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-13 09:34:40 +0300

    I have changed the background of the navbar

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/Images/Logo.png";
-import Logo2 from '../assets/Images/Logo2.png'
-import './Header.css'
+import Logo2 from "../assets/Images/Logo2.png";
+import "./Header.css";
 export default function Header() {
   return (
     <>
-      <main className="flex p-8 justify-between items-center ">
-        <header className="ml-20 text-purple-700 text-3xl">🎬CineScope</header>
+      <main className="bg-gray-900 text-white flex justify-between p-6 ">
+        <header className="ml-20 text-3xl">🎬CineScope</header>
         <ul className="flex gap-20 pr-5  text-xl text-shadow-sm text-shadow">
           <li>
             <Link to="/">Home</Link>
