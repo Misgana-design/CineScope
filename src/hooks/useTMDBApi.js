d36d59e113c253910d42dd340728edcca1d3f820
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-13 09:45:17 +0300

    I have exported the function without using default

--- src/hooks/useTMDBApi.js
+++ src/hooks/useTMDBApi.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"; // good for posters
 export const ORIGINAL_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"; // good for hero banners
 
-export default async function fetchTrending() {
+export async function fetchTrending() {
   try {
     const res = await axios.get(
       `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
