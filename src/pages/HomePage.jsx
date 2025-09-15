161aa585bfb1405150c774d92860293164f2b3be
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-15 11:38:05 +0300

    I have changed the color of the movie title and I made the image container flexible with the images that are coming with different aspect ratio

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-
-import { fetchTrending, ORIGINAL_IMAGE_BASE_URL } from "../hooks/useTMDBApi"; // good for hero banners
+import { fetchTrending, ORIGINAL_IMAGE_BASE_URL } from "../hooks/useTMDBApi";
+import MovieCard from "../components/MovieCard";
 
 export default function HomePage() {
   const {
@@ -15,21 +15,55 @@ export default function HomePage() {
     refetchOnWindowFocus: true,
     retry: 3,
   });
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Something went wrong</p>;
+  if (isLoading)
+    return <p className="text-white text-center text-2xl">Loading...</p>;
+  if (error)
+    return (
+      <p className="text-white text-center text-2xl">Something went wrong</p>
+    );
   const heroMovies = movie[0];
   const restMovies = movie.slice(1);
   return (
     <>
       {isFetching && <p>Background refreshing</p>}
-      <div>
-        {heroMovies && <p>{heroMovies.title}</p>}
-        <div
-          style={{
-            backgroundImage: `url(${ORIGINAL_IMAGE_BASE_URL}${heroMovies.backdrop_path})`,
-            backgroundSize: "cover",
-          }}
-        ></div>
+      <div className="mt-6">
+        {heroMovies && (
+          <>
+            <div className="flex justify-between items-center ">
+              <div
+                style={{
+                  backgroundImage: `url(${ORIGINAL_IMAGE_BASE_URL}${heroMovies.backdrop_path})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center center",
+                  width: "100%",
+                  aspectRatio: "16 / 9",
+                  maxHeight: "500px",
+                }}
+                className="shadow-2xl shadow-gray-700 ml-28 object-cover"
+              ></div>
+              <div className="p-28">
+                <p className="bg-linear-to-r from-blue-500 to-green-500 text-transparent bg-clip-text text-7xl font-extrabold">
+                  {heroMovies.title}
+                </p>
+                <p className="text-white w-100 mt-4 font-mono">
+                  {heroMovies.overview}
+                </p>
+                <button className="bg-green-400  cursor-pointer w-30 h-10 text-lg font-bold rounded-lg mt-4 border-solid border-2 border-green-400 hover:bg-black hover:text-white">
+                  Watch now
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+
+      <div className="text-white font-extrabold text-3xl">
+        Trending This Week
+        <div>
+          {restMovies.map((restMovie) => (
+            <MovieCard key={restMovie.id} movie={restMovie} />
+          ))}
+        </div>
       </div>
     </>
   );
