48ed19e5e64d5d5924f66c7327292f9e8eeef0b1
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-15 11:35:19 +0300

    I have made the navbar fixed

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
-import Logo from "../assets/Images/Logo.png";
-import Logo2 from "../assets/Images/Logo2.png";
 import "./Header.css";
 export default function Header() {
   return (
     <>
-      <main className="bg-black text-white flex justify-between p-6 ">
-        <header className="ml-20 text-green-400 text-3xl">🎬CineScope</header>
+      <main className="fixed top-0 left-0 right-0 bg-black text-white flex justify-between p-8 ">
+        <header className="ml-20 text-green-400 text-3xl font-extrabold font-mono">
+          🎬CineScope
+        </header>
         <ul className="flex gap-20 pr-5  text-xl text-shadow-sm text-shadow">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="py-2 px-2 hover:bg-gray-700">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/Search">Search</Link>
+            <Link to="/Search" className="py-2 px-2 hover:bg-gray-700">
+              Search
+            </Link>
           </li>
           <li>
-            <Link to="/Favorites">Favorites</Link>
+            <Link to="/Favorites" className="py-2 px-2 hover:bg-gray-700">
+              Favorites
+            </Link>
           </li>
         </ul>
       </main>
