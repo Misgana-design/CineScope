c44323ac3f3101c9d7b8ff76abbdd7de32be84e3
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-19 14:51:49 +0300

    Created search page UI

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,18 @@
 export default function SearchPage() {
   return (
     <>
-      <h1>SearchPage</h1>
+      <div className="flex justify-center">
+        <div className="mt-30">
+          <h1 className=" text-green-400 font-extrabold text-7xl">
+            🎬CineScope
+          </h1>
+          <input
+            type="text"
+            placeholder="🔍︎Search for movies"
+            className="mt-25 ml-5 pl-3 text-black font-medium rounded-4xl focus:outline-none focus:border-2 focus:border-green-600 bg-white w-100 h-12"
+          />
+        </div>
+      </div>
     </>
   );
 }
