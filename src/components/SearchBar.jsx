dba0de49aa418d67d007eb00e89d7c0f8eda6779
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-20 20:57:59 +0300

    I have added form to handle searching

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,7 @@
 export default function SearchBar() {
+  function handleSearch(data) {
+    const result = data.get("movie");
+  }
   return (
     <>
       <div className="flex justify-center">
@@ -6,12 +9,14 @@ export default function SearchBar() {
           <h1 className=" text-green-400 font-extrabold text-7xl">
             🎬CineScope
           </h1>
-          <input
-            type="text"
-            name="movie"
-            placeholder="🔍︎Search for movies"
-            className="mt-25 ml-5 pl-3 text-black font-medium rounded-4xl focus:outline-none focus:border-2 focus:border-green-600 bg-white w-100 h-12"
-          />
+          <form action={handleSearch}>
+            <input
+              type="text"
+              name="movie"
+              placeholder="🔍︎Search for movies"
+              className="mt-25 ml-5 pl-3 text-black font-medium rounded-4xl focus:outline-none focus:border-2 focus:border-green-600 bg-white w-100 h-12"
+            />
+          </form>
         </div>
       </div>
     </>
