9c91f9c358e021456ba52b3e7573915381ede009
Misgana-design <misganawoldeselassie@gmail.com>
2025-09-27 14:49:25 +0300

    rounded

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -29,7 +29,7 @@ export default function SearchBar({ setQuery }) {
               placeholder="🔍︎ Search for movies"
               className="mt-20 ml-6 pl-3 text-black font-medium rounded-4xl focus:outline-none focus:border-2 focus:border-green-600 bg-white w-90 h-12"
             />
-            <button className="bg-blue-500">Search</button>
+            <button className="bg-blue-500 rounded-4xl">Search</button>
           </form>
         </div>
       </div>
