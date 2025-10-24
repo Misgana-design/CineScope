ad716888bc2041a41daf6fb4d59363305d8d095f
Misgana-design <misganawoldeselassie@gmail.com>
2025-10-24 21:32:20 +0300

    HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -111,7 +111,7 @@ export default function HomePage() {
           </div>
         </div>
         <div className="mt-20">
-          Trending TV this week series
+          Trending TV series this week
           <div className="grid grid-cols-5 w-250 gap-6 mt-5 aspect-auto relative">
             {tv.map((tvSeries) => {
               const isFavorite = isFavorites(tvSeries);
